003fb1a13a50bd8288555e028630d42064cfd0ec
Ulisesnunez <73852141+Ulisesnunez@users.noreply.github.com>
2022-09-16 17:02:44 -0300

    INSERTS DB

--- myu.sql
+++ myu.sql
@@ -52,10 +52,131 @@ CREATE TABLE `carrito` (
 CREATE TABLE `catalogo` (
   `nro_producto` int(11) NOT NULL,
   `producto` varchar(100) DEFAULT NULL,
-  `talle` char(1) DEFAULT NULL,
+  `talle` varchar(5) DEFAULT NULL,
   `colegio` varchar(30) DEFAULT NULL,
   `stock` int(11) DEFAULT NULL,
   `precio` int(6) DEFAULT NULL
+
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('1','Chomba Juan XXIII','6','Juan XXIII',50, 3000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('1','Chomba Juan XXIII','8','Juan XXIII',50, 3000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('1','Chomba Juan XXIII','10','Juan XXIII',50, 3000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('1','Chomba Juan XXIII','12','Juan XXIII',50, 3000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('1','Chomba Juan XXIII','14','Juan XXIII',50, 3000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('1','Chomba Juan XXIII','16','Juan XXIII',50, 3000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('1','Chomba Juan XXIII','s','Juan XXIII',50, 3000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('1','Chomba Juan XXIII','m','Juan XXIII',50, 3000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('1','Chomba Juan XXIII','l','Juan XXIII',50, 3000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('1','Chomba Juan XXIII','xl','Juan XXIII',50, 3000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('1','Chomba Juan XXIII','xxl','Juan XXIII',50, 3000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('1','Chomba Juan XXIII','xxxl','Juan XXIII',50, 3000)
+
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('2','Remera Juan XXIII','6','Juan XXIII',50,2900);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('2','Remera Juan XXIII','8','Juan XXIII',50,2900);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('2','Remera Juan XXIII','10','Juan XXIII',50,2900);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('2','Remera Juan XXIII','12','Juan XXIII',50,2900);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('2','Remera Juan XXIII','14','Juan XXIII',50,2900);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('2','Remera Juan XXIII','16','Juan XXIII',50,2900);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('2','Remera Juan XXIII','s','Juan XXIII',50,2900);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('2','Remera Juan XXIII','m','Juan XXIII',50,2900);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('2','Remera Juan XXIII','l','Juan XXIII',50,2900);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('2','Remera Juan XXIII','xl','Juan XXIII',50,2900);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('2','Remera Juan XXIII','xxl','Juan XXIII',50,2900);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('2','Remera Juan XXIII','xxxl','Juan XXIII',50,2900);
+
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('3','Campera de invierno Juan XXIII','6','Juan XXIII',50,5000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('3','Campera de invierno Juan XXIII','8','Juan XXIII',50,5000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('3','Campera de invierno Juan XXIII','10','Juan XXIII',50,5000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('3','Campera de invierno Juan XXIII','12','Juan XXIII',50,5000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('3','Campera de invierno Juan XXIII','14','Juan XXIII',50,5000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('3','Campera de invierno Juan XXIII','16','Juan XXIII',50,5000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('3','Campera de invierno Juan XXIII','s','Juan XXIII',50,5000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('3','Campera de invierno Juan XXIII','m','Juan XXIII',50,5000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('3','Campera de invierno Juan XXIII','l','Juan XXIII',50,5000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('3','Campera de invierno Juan XXIII','xl','Juan XXIII',50,5000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('3','Campera de invierno Juan XXIII','xxl','Juan XXIII',50,5000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('3','Campera de invierno Juan XXIII','xxxl','Juan XXIII',50,5000);
+
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('4','Campera de Verano Juan XXIII','6','Juan XXIII',5,4500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('4','Campera de Verano Juan XXIII','8','Juan XXIII',5,4500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('4','Campera de Verano Juan XXIII','10','Juan XXIII',5,4500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('4','Campera de Verano Juan XXIII','12','Juan XXIII',5,4500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('4','Campera de Verano Juan XXIII','14','Juan XXIII',5,4500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('4','Campera de Verano Juan XXIII','16','Juan XXIII',5,4500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('4','Campera de Verano Juan XXIII','s','Juan XXIII',5,4500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('4','Campera de Verano Juan XXIII','m','Juan XXIII',5,4500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('4','Campera de Verano Juan XXIII','l','Juan XXIII',5,4500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('4','Campera de Verano Juan XXIII','xl','Juan XXIII',5,4500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('4','Campera de Verano Juan XXIII','xxl','Juan XXIII',5,4500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('4','Campera de Verano Juan XXIII','xxxl','Juan XXIII',5,4500);
+
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('5','Buzo Juan XXIII','6','Juan XXIII',5,3500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('5','Buzo Juan XXIII','8','Juan XXIII',5,3500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('5','Buzo Juan XXIII','10','Juan XXIII',5,3500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('5','Buzo Juan XXIII','12','Juan XXIII',5,3500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('5','Buzo Juan XXIII','14','Juan XXIII',5,3500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('5','Buzo Juan XXIII','16','Juan XXIII',5,3500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('5','Buzo Juan XXIII','s','Juan XXIII',5,3500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('5','Buzo Juan XXIII','m','Juan XXIII',5,3500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('5','Buzo Juan XXIII','l','Juan XXIII',5,3500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('5','Buzo Juan XXIII','xl','Juan XXIII',5,3500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('5','Buzo Juan XXIII','xxl','Juan XXIII',5,3500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('5','Buzo Juan XXIII','xxl','Juan XXIII',5,3500);
+
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('6','Pantalón de invierno','6','Juan XXXII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('6','Pantalón de invierno','8','Juan XXXII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('6','Pantalón de invierno','10','Juan XXXII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('6','Pantalón de invierno','12','Juan XXXII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('6','Pantalón de invierno','14','Juan XXXII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('6','Pantalón de invierno','16','Juan XXXII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('6','Pantalón de invierno','s','Juan XXXII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('6','Pantalón de invierno','m','Juan XXXII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('6','Pantalón de invierno','l','Juan XXXII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('6','Pantalón de invierno','xl','Juan XXXII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('6','Pantalón de invierno','xxl','Juan XXXII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('6','Pantalón de invierno','xxxl','Juan XXXII',5,4000);
+
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('7','Pantalón de Verano','6','Juan XXIII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('7','Pantalón de Verano','8','Juan XXIII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('7','Pantalón de Verano','10','Juan XXIII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('7','Pantalón de Verano','12','Juan XXIII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('7','Pantalón de Verano','14','Juan XXIII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('7','Pantalón de Verano','16','Juan XXIII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('7','Pantalón de Verano','s','Juan XXIII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('7','Pantalón de Verano','m','Juan XXIII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('7','Pantalón de Verano','l','Juan XXIII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('7','Pantalón de Verano','xl','Juan XXIII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('7','Pantalón de Verano','xxl','Juan XXIII',5,4000);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('7','Pantalón de Verano','xxxl','Juan XXIII',5,4000);
+
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('8','Conjunto de Invierno','6','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('8','Conjunto de Invierno','8','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('8','Conjunto de Invierno','10','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('8','Conjunto de Invierno','12','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('8','Conjunto de Invierno','14','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('8','Conjunto de Invierno','16','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('8','Conjunto de Invierno','s','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('8','Conjunto de Invierno','m','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('8','Conjunto de Invierno','l','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('8','Conjunto de Invierno','xl','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('8','Conjunto de Invierno','xxl','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('8','Conjunto de Invierno','xxxl','Juan XXIII',5,8500);
+
+
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('9','Conjunto de Verano','6','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('9','Conjunto de Verano','8','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('9','Conjunto de Verano','10','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('9','Conjunto de Verano','12','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('9','Conjunto de Verano','14','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('9','Conjunto de Verano','16','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('9','Conjunto de Verano','s','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('9','Conjunto de Verano','m','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('9','Conjunto de Verano','l','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('9','Conjunto de Verano','xl','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('9','Conjunto de Verano','xxl','Juan XXIII',5,8500);
+INSERT INTO `catalogo`(`nro_producto`, `producto`, `talle`, `colegio`, `stock`, `precio`) VALUES ('9','Conjunto de Verano','xxxl','Juan XXIII',5,8500);
+
+
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
